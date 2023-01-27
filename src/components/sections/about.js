7daf740a1800c60adb2ca8799196c82dd17398b1
Jorge Cobo <cobo-angel@hotmail.com>
2023-01-27 07:48:00 -0400

    About section personalized

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['C++', 'C#', 'Rust', 'Unity', 'JavaScript', 'HLSL/GLSL'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,12 +135,16 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Jorge and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Jorge and I’ve been professionally working as a 
+              Game Developer for 3 to 4 years, but I’ve been developing games 
+              (Personal projects, or for fun) in Unity for five years. 
+              I’ve mainly worked on mobile games in which the focus is more on a single-core 
+              gameplay mechanic or puzzles. I have also worked on some Mid-Core games 
+              developing Procedural Generation systems, Physics systems, Rendering systems, etc... 
+              And developing APIs and tools; UI managers, Animation tools, and Level editors for Unity 
+              to help the team (Artists, Game Designers or other Game Developers) to accomplish their goals.
             </p>
-
+{ /*
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
               <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
@@ -160,7 +164,7 @@ const About = () => {
               that covers everything you need to build a web app with the Spotify API using Node
               &amp; React.
             </p>
-
+  */}
             <p>Here are a few technologies I’ve been working with recently:</p>
           </div>
 
